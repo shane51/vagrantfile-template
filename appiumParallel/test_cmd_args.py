432a5389f8749bc00b553d07b505322259f79d01
shane51 <suse51@163.com>
2017-12-31 22:32:27 +0800

    support argument parser

--- appiumParallel/test_cmd_args.py
+++ appiumParallel/test_cmd_args.py
@@ -1,3 +1,12 @@
-import sys
+import argparse
 
-print(sys.argv[1:])
+parser = argparse.ArgumentParser(description='Run test in docker')
+
+parser.add_argument(dest='filenames', metavar='filename', nargs='*')
+parser.add_argument('-D', '--dir', metavar='Directory', required=True,
+                    dest='directory', action='append',
+                    help='root directory of test file')
+
+args = parser.parse_args()
+print(args.directory[0])
+print(args.filenames[0])
